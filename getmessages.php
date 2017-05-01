310fe415820158b33a02df42620d0fb3f7b2dd5e
Muhammed Mishal <muhdmishal@gmail.com>
2017-05-01 19:22:49 +0530

    test code

--- getmessages.php
+++ getmessages.php
@@ -1,10 +1,10 @@
 <?php
-include 'dbc.php';
+
 function getMessagesCount()
 {
 	$query = "SELECT *  FROM `message` WHERE `receiver_id` = $_SESSION[user_id] AND `viewedstatus` = 1 ";
 
-  //$link = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+  $link = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 	$messages = mysqli_query($link,$query);
 
 	return mysqli_num_rows($messages);
