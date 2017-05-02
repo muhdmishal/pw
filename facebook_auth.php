b53876da1b08693fa6c267dd880c3a19abc3445b
Muhammed Mishal <muhdmishal@gmail.com>
2017-05-02 22:27:47 +0530

    test

--- facebook_auth.php
+++ facebook_auth.php
@@ -24,7 +24,18 @@ if(empty($code)) {
 $token_url = "https://graph.facebook.com/oauth/access_token?"
 	. "client_id=" . $app_id . "&redirect_uri=" . urlencode($my_url)
 	. "&client_secret=" . $app_secret . "&code=" . $code . "&scope=publish_stream,email";
-	$response = file_get_contents($token_url);
+	//$response = file_get_contents($token_url);
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+  // This is what solved the issue (Accepting gzip encoding)
+  curl_setopt($ch, CURLOPT_ENCODING, "gzip,deflate");
+  $response = curl_exec($ch);
+  curl_close($ch);
+  echo $response;
+
   echo "sdf";
   echo "<pre>";
   print_r($response);
